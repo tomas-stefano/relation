d97e0b1dd51ae81503b4666491697a1cb9f0423f
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-11-16 13:04:13 -0200

    Defining the Relation module (Let's get ready to Rumble!!) =p

--- wrappers/ruby/ext/relation_wrapper/relation_wrapper.c
+++ wrappers/ruby/ext/relation_wrapper/relation_wrapper.c
@@ -1,5 +1,5 @@
 #include <ruby.h>
 
 void Init_relation_wrapper() {
-
+	VALUE relation_module = rb_define_module("Relation");
 }
--- wrappers/ruby/spec/relation/relation_wrapper_spec.rb
+++ wrappers/ruby/spec/relation/relation_wrapper_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
 
-module Relation
+describe Relation do
+  
 end
